a79cd5b0d109cbb5c125379d7262feda60b35473
Alex Luke <banduin@gmail.com>
2011-09-29 01:41:08 -0700

    Make app more modular.
    Also has the side effect of looking like a django app, so heroku is tricked into giving it a database

--- replays/views.py
+++ replays/views.py
@@ -1,50 +1,46 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for
 from hashlib import sha1
 import model
 
-app = Flask(__name__)
+views = Blueprint('views', __name__, static_folder='../static', template_folder='../templates')
 
-@app.route('/')
+@views.route('/')
 def home():
 	latest = []
 	for replay in model.Replay.query.all():
 		latest.append({
 			'name': replay.original_filename,
-			'link': url_for('replay', key=replay.key),
+			'link': url_for('.replay', key=replay.key),
 		})
 	
 	return render_template('home.html', latest=latest)
 
-@app.route('/replay/<key>/')
+@views.route('/replay/<key>/')
 def replay(key):
 	replay = model.Replay.query.filter_by(key=key).one()
 	return render_template('replay.html', replay=replay)
 
-@app.route('/replay/<key>/download/')
+@views.route('/replay/<key>/download/')
 def replay_download(key):
 	replay = model.Replay.query.filter_by(key=key).one()
 	return redirect(replay.download_url())
 
-@app.route('/submit', methods=['POST'])
+@views.route('/submit', methods=['POST'])
 def submit():
 	file = request.files['file']
 	hash = sha1(file.read()).hexdigest()
 	replay = model.Replay(file.filename, hash)
 
 	if model.Replay.query.filter_by(key=replay.key).count() > 0:
-		return redirect(url_for('replay', key=replay.key))
+		return redirect(url_for('.replay', key=replay.key))
 
 	replay.upload(file)
 	model.db.session.add(replay)
 	model.db.session.commit()
-	return redirect(url_for('replay', key=replay.key))
+	return redirect(url_for('.replay', key=replay.key))
 
-@app.errorhandler(404)
+@views.errorhandler(404)
 def page_not_found(error):
 	return render_template('404.html'), 404
 
-if __name__ == '__main__':
-	port = int(os.environ.get('PORT', 5000))
-	app.debug = os.environ.get('DEBUG', False)
-	app.run(host='0.0.0.0', port=port)
---
+++ runserver.py
@@ -0,0 +1,7 @@
+import os
+from replays import create_app
+
+app = create_app()
+port = int(os.environ.get('POST', 5000))
+app.debug = os.environ.get('DEBUG', False)
+app.run(host='0.0.0.0', port=port)
