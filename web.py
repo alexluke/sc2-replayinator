b251a395b08960c9cf90281750230be7ebdd6a85
Alex Luke <banduin@gmail.com>
2011-09-28 19:59:44 -0700

    File upload form

--- web.py
+++ web.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return "Hi there"
+	return render_template('home.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+	file = request.files['file']
+	filename = secure_filename(f.filename)
+	return filename
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
+	app.debug = True
 	app.run(host='0.0.0.0', port=port)
