679775824d3b71d4e2098e3c639a23e53bc48569
Alex Luke <banduin@gmail.com>
2011-09-28 22:51:53 -0700

    Upload files to S3

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flaskext.sqlalchemy import SQLAlchemy
+import boto
+import settings
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://///tmp/test.db'
@@ -9,12 +11,16 @@ class Replay(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	key = db.Column(db.String(20), unique=True)
 	hash = db.Column(db.String(40), unique=True)
-	filename = db.Column(db.String(255))
+	original_filename = db.Column(db.String(512))
+	filepath = db.Column(db.String(1024))
 
 	def __init__(self, filename, hash):
-		self.filename = filename
+		self.original_filename = filename
 		self.hash = hash
 		self.key = self.create_unique_key()
+
+	def get_filename(self):
+		return self.key + '.SC2Replay'
 	
 	def create_unique_key(self):
 		existing = self.query.filter_by(hash=self.hash).first()
@@ -23,9 +29,17 @@ class Replay(db.Model):
 
 		length = 6
 		key = self.hash[0:length]
-		while query.filter_by(key=key).count() > 0:
+		while self.query.filter_by(key=key).count() > 0:
 			length += 1
 			if length > 40:
 				break;
 			key = self.hash[0:length]
 		return key
+	
+	def upload(self, file):
+		s3 = boto.connect_s3(settings.AWS_ACCESS_KEY_ID, settings.AWS_SECRET_ACCESS_KEY)
+		bucket = s3.get_bucket(settings.S3_BUCKET)
+		s3_file = boto.s3.key.Key(bucket)
+		s3_file.key = self.get_filename()
+		s3_file.set_contents_from_file(file)
+		self.filepath = s3_file.key
--- web.py
+++ web.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for
-from werkzeug import secure_filename
 from hashlib import sha1
 import model
 
@@ -11,7 +10,7 @@ def home():
 	latest = []
 	for replay in model.Replay.query.all():
 		latest.append({
-			'name': replay.filename,
+			'name': replay.original_filename,
 			'link': url_for('replay', key=replay.key),
 		})
 	
@@ -20,20 +19,21 @@ def home():
 @app.route('/replay/<key>')
 def replay(key):
 	replay = model.Replay.query.filter_by(key=key).one()
-	return replay.filename
+	return replay.original_filename
 
 @app.route('/submit', methods=['POST'])
 def submit():
 	file = request.files['file']
 	hash = sha1(file.read()).hexdigest()
-	replay = model.Replay(secure_filename(file.filename), hash)
+	replay = model.Replay(file.filename, hash)
 
 	if model.Replay.query.filter_by(key=replay.key).count() > 0:
 		return redirect(url_for('replay', key=replay.key))
 
+	replay.upload(file)
 	model.db.session.add(replay)
 	model.db.session.commit()
-	return replay.key
+	return redirect(url_for('replay', key=replay.key))
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
